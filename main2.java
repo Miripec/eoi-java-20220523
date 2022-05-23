f552fbc437683c2cb5b2ac33362e2c85190674a0
Miripec <miriam.pcj@gmail.com>
2022-05-23 17:22:17 +0200

    Rama

--- main2.java
+++ main2.java
@@ -1,5 +1,12 @@
 public class main2 {
     public static void main(String[] args) {
-        System.out.println("Vaaale vale");
+        imprimeMil();
+
+    }
+
+    public static void imprimeMil(){
+        for(int i=1; i<=1000; i++){
+            System.out.println(i);
+        }
     }
 }
