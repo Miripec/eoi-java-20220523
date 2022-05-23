6b58858cbc586b357ceedfb659777bbb11618e0b
Miripec <miriam.pcj@gmail.com>
2022-05-23 17:35:34 +0200

    Rama cambiada

--- main2.java
+++ main2.java
@@ -1,12 +1,6 @@
 public class main2 {
     public static void main(String[] args) {
-        imprimeMil();
+        Counter.counter();
 
-    }
-
-    public static void imprimeMil(){
-        for(int i=1; i<=1000; i++){
-            System.out.println(i);
-        }
     }
 }
